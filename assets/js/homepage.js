c98ccf666e3a08d19c7bd20f756a6f1f602aece2
dylan fair <dylanfair11@gmail.com>
2021-09-27 18:21:47 -0500

    added searching for users

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,5 @@
+let userFormEl = document.querySelector("#user-form");
+let nameInputEl = document.querySelector("#username");
 let getUserRepos = function(user){
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
     fetch(apiUrl).then(function(response){
@@ -7,4 +9,14 @@ let getUserRepos = function(user){
     })
 
 }
-getUserRepos("facebook");
+let formSubmitHandler = function(event){
+    event.preventDefault();
+    let username = nameInputEl.value.trim();
+    if(username){
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+}
+userFormEl.addEventListener("submit", formSubmitHandler);
